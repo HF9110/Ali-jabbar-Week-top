3528d4ae73aeb707065d58da3a55374c946e14ef
HF9110 <editor.hussein@gmail.com>
2025-11-18 23:30:30 +0300

    update344

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// Import the main application file
+// التأكد من أن اسم الملف المستورد مطابق تمامًا
 import App from './AliJabbarContestManager.jsx';
 
 const container = document.getElementById('root');
