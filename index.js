bda910b89ea49be3f75263927e10c580c72696f8
HF9110 <editor.hussein@gmail.com>
2025-11-18 23:58:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // تم تغيير اسم الاستيراد لتجنب أي تعارضات محتملة في النطاق (Scope)
 import AliJabbarApp from './AliJabbarContestManager.jsx';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
