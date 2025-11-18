7ea7d79ebe45b484e84fe37153651efff2519e4c
HF9110 <editor.hussein@gmail.com>
2025-11-19 00:48:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// استدعاء الملف الرئيسي
-import App from './AliJabbarContestManager.jsx';
+// تم التحديث لاستدعاء الملف App.jsx
+import App from './App.jsx';
 
 const container = document.getElementById('root');
 if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App /> 
-    </React.StrictMode>
-  );
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App /> 
+    </React.StrictMode>
+  );
 } else {
-    console.error("Root element #root not found.");
+    console.error("Root element #root not found.");
 }
